227af96c407585e11496c12ebbcf2446c0597808
sjeguedes <sje.guedes@gmail.com>
2020-07-23 23:19:16 +0200

    added phone details resource

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 /**
@@ -16,9 +17,7 @@ use Symfony\Component\Serializer\SerializerInterface;
  *
  * Manage all requests about phones data.
  *
- * @Route({
- *     "en": "/{_locale}/phones"
- * })
+ * @Route("/{_locale}")
  */
 class PhoneController extends AbstractController
 {
@@ -51,14 +50,18 @@ class PhoneController extends AbstractController
      * @return Response
      *
      * @Route({
-     *     "en": "/list"
+     *     "en": "/phones/list"
      * }, name="list_phones", methods={"GET"})
      */
     public function listPhones(): Response
     {
         $phones = $this->phoneRepository->findAll();
         // Filter results with serialization group
-        $data = $this->serializer->serialize($phones, 'json', ['groups' => ['phone_list_read']]);
+        $data = $this->serializer->serialize(
+            $phones,
+            'json',
+            ['groups' => ['phone_list_read']]
+        );
         // Pass JSON data to response
         return new Response($data, Response::HTTP_OK, [
             'Content-Type' => 'application/json'
@@ -74,19 +77,53 @@ class PhoneController extends AbstractController
      * @return Response
      *
      * @Route({
-     *     "en": "/paginated/{page<\d+>?1}/{limit<\d+>?10}"
+     *     "en": "/phones/paginated/{page<\d+>?1}/{limit<\d+>?10}"
      * }, name="list_paginated_phones", methods={"GET"})
      *
      * @see https://www.doctrine-project.org/projects/doctrine-orm/en/2.7/tutorials/pagination.html
      */
     public function listPaginatedPhones(Request $request): Response
     {
-        $page = $request->attributes->get('page');
-        $limit =  $request->attributes->get('limit');
+        $page = (int) $request->attributes->get('page');
+        $limit = (int) $request->attributes->get('limit');
         // Find a set of Phone entities thanks to parameters and Doctrine Paginator
-        $phones = $this->phoneRepository->findPaginatedOnes((int) $page, (int) $limit);
+        $phones = $this->phoneRepository->findPaginatedOnes($page, $limit);
         // Filter results with serialization group
-        $data = $this->serializer->serialize($phones, 'json', ['groups' => ['phone_list_read']]);
+        $data = $this->serializer->serialize(
+            $phones,
+            'json',
+            ['groups' => ['phone_list_read']]
+        );
+        // Pass JSON data to response
+        return new Response($data, Response::HTTP_OK, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * Show details about a particular phone.
+     *
+     * Please note Symfony param converter can be used here to retrieve a Phone entity.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @Route({
+     *     "en": "/phone/{uuid<[\w-]{36}>}"
+     * }, name="show_phone", methods={"GET"})
+     */
+    public function showPhone(Request $request): Response
+    {
+        $uuid = $request->attributes->get('uuid');
+        $phone = $this->phoneRepository->findOneBy(['uuid' => $uuid]);
+        // Filter result with serialization group
+        $data = $this->serializer->serialize(
+            $phone,
+            'json',
+            // Exclude Offer collection since it is not expected at this time in app.
+            [AbstractNormalizer::IGNORED_ATTRIBUTES => ['offers']]
+        );
         // Pass JSON data to response
         return new Response($data, Response::HTTP_OK, [
             'Content-Type' => 'application/json'
