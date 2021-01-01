d098ce358000405e28b73ce9c1780f0e415287d4
sjeguedes <sje.guedes@gmail.com>
2021-01-01 15:43:11 +0100

    Revert "added custom Swagger UI CSS styles and template to hide Nelmio header and improve full_list query parameter UX for API documentation"
    This reverts commit 0eb682820a456c4af9fdac84c29108a57fb9fd1b.

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
